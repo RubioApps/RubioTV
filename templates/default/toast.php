03f4f2e9d24f1b0fc93a905107be329b9027330b
Jaime Rubio <155658204+RubioApps@users.noreply.github.com>
2024-01-04 19:50:11 +0100

    Add files via upload

--- templates/default/toast.php
+++ templates/default/toast.php
@@ -1,4 +1,37 @@
-<?php defined('_TVEXEC') or die;?>
+<?php 
+/**
+ +-------------------------------------------------------------------------+
+ | RubioTV  - A domestic IPTV Web app browser                              |
+ | Version 1.0.0                                                           |
+ |                                                                         |
+ | Copyright (C) The Roundcube Dev Team                                    |
+ |                                                                         |
+ | This program is free software: you can redistribute it and/or modify    |
+ | it under the terms of the GNU General Public License as published by    |
+ | the Free Software Foundation.                                           |
+ |                                                                         |
+ | This file forms part of the RubioTV software.                           |
+ |                                                                         |
+ | If you wish to use this file in another project or create a modified    |
+ | version that will not be part of the RubioTV Software, you              |
+ | may remove the exception above and use this source code under the       |
+ | original version of the license.                                        |
+ |                                                                         |
+ | This program is distributed in the hope that it will be useful,         |
+ | but WITHOUT ANY WARRANTY; without even the implied warranty of          |
+ | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the            |
+ | GNU General Public License for more details.                            |
+ |                                                                         |
+ | You should have received a copy of the GNU General Public License       |
+ | along with this program.  If not, see http://www.gnu.org/licenses/.     |
+ |                                                                         |
+ +-------------------------------------------------------------------------+
+ | Author: Jaime Rubio <jaime@rubiogafsi.com>                              |
+ +-------------------------------------------------------------------------+
+*/
+
+defined('_TVEXEC') or die;
+?>
 
 <div id="tv-toast" class="z-100 toast-container d-none">
   <div class="toast align-items-center d-flex text-white border-0 position-absolute end-0 bottom-0 m-1" role="alert" aria-live="assertive" aria-atomic="true">    
