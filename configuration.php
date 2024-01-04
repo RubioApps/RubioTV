7dffaecdaf99fcf1d99478426cab88fa67dc4124
Jaime Rubio <155658204+RubioApps@users.noreply.github.com>
2024-01-04 19:22:01 +0100

    Update configuration.php

--- configuration.php
+++ configuration.php
@@ -5,15 +5,15 @@ defined('_TVEXEC') or die;
 
 class TVConfig {
         public $sitename = 'RubioTV';
-        public $live_site = 'https://famillerubio.com/tv';
-	public $log_path = '/var/www/rubiotv/log';
-	public $tmp_path = '/var/www/rubiotv/tmp';        
+        public $live_site = 'https:/yoursite.com/tv';
+	public $log_path = '/path/to/your/site/log';
+	public $tmp_path = '/path/to/your/site/tmp';        
         public $menu = ['dtv','categories','languages','countries','custom','guides'];
 	public $list_limit = 30;
         public $theme = 'default';
         public $dtv = [
                 'type'          => 'tvheadend',                
-                'host'          => 'https://famillerubio.com/coreelec',
+                'host'          => 'https://yoursite.com/coreelec',
                 'channels'      => '/playlist/channels',
                 'stream'        => '/stream/channel',
                 'xmltv'         => '/xmltv/channels',
@@ -23,19 +23,16 @@ class TVConfig {
         public $use_cache = true;
         public $notify_cache = false;
         public $links = [
-                'RubioVPN'      => 'https://famillerubio.com/vpn/',
-                'RubioGuard'    => 'https://famillerubio.com/squid/',
-                'Alfogon'       => 'https://famillerubio.com/alfogon/',
-                'Jardiname'     => 'https://famillerubio.com/jardiname/',
-
+                'MySite1'      => 'https://yoursite1.com/',
+		'MySite2'      => 'https://yoursite2.com/',
                 ];
 	public $epg = [                
 		'enabled'	=> true,
                 'notify'	=> false,                                
-                'dir'           => '/var/www/rubiotv/epg',
+                'dir'           => '/path/to/your/site/epg',
 		'exec'		=> 'npm run grab --prefix=%s -- --channels=%s --output=%s --days=%s > /dev/null 2>&1',
                 'lock'          => 60 ,   /* seconds */
                 'expiry'        => 7 ,    /* days */		
-                'secret_key'    => 'Bw:?%3)NJFTs`^:gg:Q!6()vS!\a#Sc<[t5Z`9s9G*.3X<Y7^tRP}+!:2b88X}_', /* change this */
+                'secret_key'    => 'change-me-with-a-long-random-key', /* change this */
 	        ];
 }
