800f0c69eda7610fbab5687be35da12293daa738
Jaime Rubio <155658204+RubioApps@users.noreply.github.com>
2024-01-04 19:29:16 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,40 @@
 <?php 
+/**
+ +-------------------------------------------------------------------------+
+ | RubioTV  - A domestic IPTV Web app browser                              |
+ | Version 1.0.0                                                           |
+ |                                                                         |
+ | Copyright (C) The Roundcube Dev Team                                    |
+ |                                                                         |
+ | This program is free software: you can redistribute it and/or modify    |
+ | it under the terms of the GNU General Public License (with exceptions   |
+ | for skins) as published by the Free Software Foundation,                |
+ | either version 3 of the License, or (at your option) any later version. |
+ |                                                                         |
+ | This file forms part of the RubioTV software for which the              |
+ | following exception is added: Plugins and Skins which merely make       |
+ | function calls to the Roundcube Webmail Software, and for that purpose  |
+ | include it by reference shall not be considered modifications of        |
+ | the software.                                                           |
+ |                                                                         |
+ | If you wish to use this file in another project or create a modified    |
+ | version that will not be part of the Roundcube Webmail Software, you    |
+ | may remove the exception above and use this source code under the       |
+ | original version of the license.                                        |
+ |                                                                         |
+ | This program is distributed in the hope that it will be useful,         |
+ | but WITHOUT ANY WARRANTY; without even the implied warranty of          |
+ | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the            |
+ | GNU General Public License for more details.                            |
+ |                                                                         |
+ | You should have received a copy of the GNU General Public License       |
+ | along with this program.  If not, see http://www.gnu.org/licenses/.     |
+ |                                                                         |
+ +-------------------------------------------------------------------------+
+ | Author: Jaime Rubio <jaime@rubiogafsi.com>                              |
+ +-------------------------------------------------------------------------+
+*/
+
 define('_TVEXEC', 1);
 
 ini_set('display_errors', '1');
