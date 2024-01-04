4f83b6bfb1a34e53f056535fc93b17c613f7bc38
Jaime Rubio <155658204+RubioApps@users.noreply.github.com>
2024-01-04 19:32:21 +0100

    Update index.php

--- index.php
+++ index.php
@@ -7,18 +7,13 @@
  | Copyright (C) The Roundcube Dev Team                                    |
  |                                                                         |
  | This program is free software: you can redistribute it and/or modify    |
- | it under the terms of the GNU General Public License (with exceptions   |
- | for skins) as published by the Free Software Foundation,                |
- | either version 3 of the License, or (at your option) any later version. |
+ | it under the terms of the GNU General Public License as published by    |
+ | the Free Software Foundation.                                           |
  |                                                                         |
- | This file forms part of the RubioTV software for which the              |
- | following exception is added: Plugins and Skins which merely make       |
- | function calls to the Roundcube Webmail Software, and for that purpose  |
- | include it by reference shall not be considered modifications of        |
- | the software.                                                           |
+ | This file forms part of the RubioTV software.                           |
  |                                                                         |
  | If you wish to use this file in another project or create a modified    |
- | version that will not be part of the Roundcube Webmail Software, you    |
+ | version that will not be part of the RubioTV Software, you              |
  | may remove the exception above and use this source code under the       |
  | original version of the license.                                        |
  |                                                                         |
